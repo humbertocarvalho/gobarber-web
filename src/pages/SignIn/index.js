da2923c6d3eeb98447c802591fd7ac343a02fd61
Humberto de Carvalho <hvcarvalhobrt@gmail.com>
2019-09-12 19:24:11 -0300

    Create signin and signup.

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-// import { Container } from './styles';
+import logo from '~/assets/images/logo.svg';
 
 export default function SignIn() {
-  return <h1>Oi</h1>;
+  return (
+    <>
+      <img src={logo} alt="Gobarber" />
+      <form action="">
+        <input type="email" placeholder="Seu e-mail." />
+        <input type="password" placeholder="Sua senha secreta." />
+        <button type="submit">Acessar</button>
+
+        <Link to="/register">Criar conta</Link>
+      </form>
+    </>
+  );
 }
--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-// import { Container } from './styles';
+import logo from '~/assets/images/logo.svg';
 
 export default function SignUp() {
-  return <div />;
+  return (
+    <>
+      <img src={logo} alt="Gobarber" />
+      <form action="">
+        <input type="text" placeholder="Nome completo" />
+        <input type="email" placeholder="Seu e-mail." />
+        <input type="password" placeholder="Sua senha secreta." />
+        <button type="submit">Registrar</button>
+
+        <Link to="/">Ja é cadastrado? Acesse sua conta</Link>
+      </form>
+    </>
+  );
 }
