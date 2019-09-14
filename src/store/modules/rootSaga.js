436bcc1ad3c53f0b2be7054434b88ce78bab7e94
Humberto de Carvalho <hvcarvalhobrt@gmail.com>
2019-09-13 22:31:43 -0300

    Create user reducer.

--- src/store/modules/rootSaga.js
+++ src/store/modules/rootSaga.js
@@ -1,8 +1,8 @@
 import { all } from 'redux-saga/effects';
 
 import auth from './auth/sagas';
+import user from './user/sagas';
 
 export default function* rootSaga() {
-  console.tron.log('chegou');
-  return yield all([auth]);
+  return yield all([auth, user]);
 }
