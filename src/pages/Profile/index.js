73558db2680417c20bee488a48d0c58cb599ca3d
Humberto de Carvalho <hvcarvalhobrt@gmail.com>
2019-09-14 21:39:16 -0300

    Add update profile action.

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { Form, Input } from '@rocketseat/unform';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
+import { updateProfileRequest } from '~/store/modules/user/actions';
 import { Container } from './styles';
 
 export default function Profile() {
+  const dispatchEvent = useDispatch();
   const profile = useSelector(state => state.user.profile);
 
-  function handleSubmit(data) {}
+  function handleSubmit(data) {
+    dispatchEvent(updateProfileRequest(data));
+  }
   return (
     <Container>
       <Form initialData={profile} onSubmit={handleSubmit}>
